4c57df39a446f01e032b8ddb1df513937eb2c585
vivekpk007 <vivekpk007@gmail.com>
2016-03-02 10:56:18 +0530

    done

--- exp/fun.py
+++
@@ -1,6 +0,0 @@
-def distance_from_zero(num):
-    if type(num) == int or type(num) == float:
-        return abs(num)
-    else:
-        return "Nope"
-print distance_from_zero(-5.255),
--- exp/list_arry.py
+++
@@ -1,4 +0,0 @@
-my_list = [1,9,3,8,5,7]
-
-for number in my_list:
-    print 2*number
